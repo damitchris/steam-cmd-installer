92310fe342c01f71bc2e0617fa70f6b0dd8ee9ae
Erik <erik.lg@student.intsch.se>
2016-03-23 13:27:09 +0100

    will now be able to update from the cloud

--- steamcmdinstaller3.8.sh
+++ steamcmdinstaller3.8.sh
@@ -12,6 +12,22 @@ chkhash=""
 archit=""
 file="steamcmd.sh"
 
+echo checking update
+
+git clone https://github.com/Gousaid67/steam-cmd-installer /home/$USER/cmdup
+
+cmp -s $0 /home/$USER/cmdup/steamcmdinstaller3.8.sh > /dev/null
+if [ $? -eq 1 ]; then
+ echo -a updating....
+ mkdir /home/$USER/oldsh
+ mv $0 /home/$USER/oldsh
+ mv /home/$USER/cmdup/steamcmdinstaller3.8.sh /home/$USER/
+ mv steamcmdintaller3.8 $0
+ ./$0
+else
+    echo you are up to date
+fi
+
 function getInput()
 {
   local rez=""
@@ -34,132 +50,14 @@ select mai in "Maintenance" "Install"; do
 done
 if test "$mai" == "1"
 then
- echo  this will help you maintain the server such as update functions, backups,, this code will contain modified code of my own, so fell free to republish it as long that you give me credit. 
- echo ------- what would you like to do? backup or update -------
-     select functions in "Update" "Backup"; do
-     case $functions in
-        Update ) functions=1 
-                     break ;;
-        Backup ) functions=2 
-                      break ;;
-    esac
- done
- 
- if test "$functions" == "2"  
- then 
- now=$(date +"%m-%d-%y")
- if [ -z "$now" ]; then
-   echo "an error occured while trying to retrieve the date, do it without a date? y or n"
-   read -r dateerr
-   while [ -z "$dateerr" ]; do
-     echo ------ Please give an input ------
-     read -r dateerr
-   done
-   if test "$dateerr" = "y"
-   then
-    echo we will generate a random number for the file
-    now= $RANDOM
-    daterr= yes
-   else
-    echo we are sorry it didnt worked, you can go to the github page to report it
-    exit
-   fi
- fi
- awnser=y
- if test "$awnser" = "y"
- then
-   echo in which folder your server resides,specify the full path!
-   read -r path
-   while [ -z "$path" ]; do
-     echo ------ Please give a path ------
-     read -r path
-   done
-    notown=1
-   fi
-   echo OK, would you like it hidden from the user?you will need to do ls -a to see it. y or n
-   read -r cloak
-   while [ -z "$cloak" ]; do
-     echo ------ Please give a awnser ------
-     read -r cloak
-   done
-   if test "$cloak" = "y"; then
-     mkdir ~/.backup
-     cd ~/.backup
-     tar -cvzf Backup_$now.tar.gz $path
-     touch sums_$now.txt
-     md5sum Backup_$now.tar.gz  | cut -c -32 > sums_$now.txt
-     oldy=y
-     if test "$oldy" = "y"
-     then
-       touch olddir_$now.txt
-       echo $path > olddir_$now.txt
-       echo old directory saved
-     fi
-   else
-     mkdir ~/backup
-     cd ~/backup
-     tar -cvzf Backup_$now.tar.gz $path
-     touch sums_$now.txt
-     md5sum Backup_$now.tar.gz | cut -c -32 >  sums_$now.txt
-     oldy=y
-     if test "$oldy" = "y"
-     then
-       touch olddir_$now.txt
-       echo $path > olddir_$now.txt
-       echo old directory saved
-     fi
-   fi
- fi
- if test "$functions" == "1" 
- then
-  echo put the appid of the server
-  read -r appid
-  while [ -z "$appid" ]; do
-   echo ------ Please give an input ------
-   read -r appid
-  done
- 
-  echo please input a username, you can log as anonymous
-  while [ -z "$username" ]; do
-   echo ------ Please give an input ------
-   read -r username
-  done
-  if test "$username" == "anonymous"
-  then
-   echo the script will log as anonymous
-  else 
-   echo input your password
-   read -r password
-   while [ -z "$password" ]; do
-     echo ------ Please give an input ------
-     read -r password
-   done
-  fi
-  if [ -d "steamcmd" ]; then
-   echo steamcmd exist
-   cd /home/$USER/steamcmd
-  else
-   echo "steamcmd does not exist do you wish to install steamcmd?y or n"
-   read -r steamcm
-   while [ -z "$steamcm" ]; do 
-    echo please try again
-    read -r steamcm
-   done
-   if test "$steamcm" == "y"
-   then
-     echo ------- Downloading steam -------
-     mkdir /home/$USER/steamcmd
-     cd /home/$USER/steamcmd
-     wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-     tar -xvzf steamcmd_linux.tar.gz
-     chmod +x steamcmd.sh
-   fi
-  fi
-  ./steamcmd.sh +login $username $password +app_update $appid +quit
-  exit 0
- fi
+ sudo apt-get install git 
+home/$USER/cmdmaint
+ mv  /home/$USER/cmdmaint/servermaintenance.sh /home/$USER/
+ chmod +x servermaintenance.sh
+ rm -rf /home/$USER/cmdmaint
+ ./servermaintenance.sh
+ exit
 fi
- 
 
 echo ------------ This script installs SteamCMD dedicated servers ------------
 
